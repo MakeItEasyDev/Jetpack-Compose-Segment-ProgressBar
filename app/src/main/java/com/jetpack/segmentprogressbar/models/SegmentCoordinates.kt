2345c0a1e6708c81b0e269319c1929efda0f7ed4
MakeItEasyDev <89842411+MakeItEasyDev@users.noreply.github.com>
2022-01-31 07:36:46 +0530

    Add files via upload

---
+++ app/src/main/java/com/jetpack/segmentprogressbar/models/SegmentCoordinates.kt
@@ -0,0 +1,8 @@
+package com.jetpack.segmentprogressbar.models
+
+data class SegmentCoordinates(
+    val topLeftX: Float,
+    val topRightX: Float,
+    val bottomLeftX: Float,
+    val bottomRightX: Float
+)
